5cddcc395973866bb19fb9498ca89b56169cc51a
wxl <2529857476@qq.com>
2021-08-18 22:51:34 +0800

    first commit

---
+++ src/main/java/com/wxl/test/Test.java
@@ -0,0 +1,13 @@
+package com.wxl.test;
+
+/**
+ * @auther:wxl
+ * @Date:2021/8/18-08-18-22:49
+ * @Description:com.wxl.test
+ * @Version:1.0
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello,git1");
+    }
+}
